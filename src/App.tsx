429ca5d833ed0c0ff841671b9ff15e920bdbf85f
NigelMarshal <nigelmarshal@gmail.com>
2021-10-27 17:33:48 +0400

    chore(app): Add Vite template starter files and config

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="bg-white">
+      <div className="py-16 sm:py-24 px-4 sm:px-6 lg:px-8 mx-auto max-w-screen-xl">
+        <div className="text-center">
+          <h2 className="text-base font-semibold tracking-wide text-blue-600 uppercase">
+            Pickle Riiiiiick
+          </h2>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
